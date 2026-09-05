1ae7290d0206c8d20f521381941dcf4dc35eac93
Ritwik Prasad <ritwikprasad142@gmail.com>
2026-09-06 04:38:27 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1, 0);
+        int offset = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == offset * 2)
+                offset = i;
+
+            res[i] = 1 + res[i - offset];
+        }
+
+        return res;
+    }
+};
