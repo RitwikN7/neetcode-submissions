a084aff3e6d2fdb438652e2416120f69d0058e8c
Ritwik Prasad <ritwikprasad142@gmail.com>
2026-09-12 23:44:34 +0530

    Add: power-of-two - submission-1

---
+++ Data Structures & Algorithms/power-of-two/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+
+        n = n & (n - 1);
+        if (n > 0)
+            return false;
+
+        return true;
+    }
+};
